7d9f2936c85d891eb9a1f8d712f66e0a7ef09da3
Zethess <vibasilio@alumnos.unex.es>
2023-08-17 10:30:35 +0200

    js finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,25 @@
-$(document).ready(function() {
-    // Capturar el evento de envío del formulario
-    $("#ratingForm").submit(function(event) {
-      event.preventDefault(); // Prevenir la recarga de la página
-  
-      // Aquí puedes realizar cualquier lógica adicional necesaria, como enviar datos al servidor
-  
-      // Cambiar las clases para activar la animación y mostrar el mensaje de agradecimiento
-     
-    });
-    
-    // Capturar el clic del botón de cambio de estado
-    $("#flipButton").click(function() {
-        $(".card").toggleClass("flip");
-    });
-  });
-//   .rating-card {
-//     @include childs-card-setup(0deg);
-    
-    
-// }
-// .thank-you-card {
-//     @include childs-card-setup(180deg);
-     
-// }
-// .card.flip .rating-card {
-//     transition: transform 0.5s;
-//     transform: rotateY(180deg);
-//   }
-  
-//   .card.flip .thank-you-card {
-//     transition: transform 0.5s;
-//     transform: rotateY(0deg);
-//   }
+class TemplateManager {
+
+  loadEventListeners() {
+    $(document).ready(() => {
+      $("#ratingForm").submit((event) => {
+        event.preventDefault(); // Prevenir la recarga de la página
+        this.changeThankYouCardInputValue($(event.currentTarget));
+      });
+      $("#flipButton").click(function() {
+          $(".card").toggleClass("flip");
+      });
+    }); 
+  }
+  changeThankYouCardInputValue(form){
+    var formRatingValue=form.find("input[name='rating']:checked").val();
+    const cardElement = form.closest('.card'); 
+    const thankYouCardSelectedSpan = cardElement.find('.thank-you-card .selected-rating-paragraph span');
+    thankYouCardSelectedSpan.text(formRatingValue !== undefined ? formRatingValue : '3');
+  }
+  init() {
+    this.loadEventListeners();
+}
+}
+const templateManager = new TemplateManager();
+templateManager.init();
